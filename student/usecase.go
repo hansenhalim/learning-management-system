59e18eda819a9b89df01bb4190169c21b7ed8456
Hansen Halim <fpsecond.hh@gmail.com>
2025-03-10 09:27:20 +0700

    wip

--- student/usecase.go
+++ student/usecase.go
@@ -1,1 +1,9 @@
 package student
+
+import "learning-management-system/entity"
+
+//go:generate mockery --name StudentRepository
+type StudentRepository interface {
+	Find(id uint) (*entity.Student, error)
+	Save(student *entity.Student) error
+}
